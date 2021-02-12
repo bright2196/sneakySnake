27343851f150f0bc0eb498f0d40de1b26256a278
Bright2196 <claireriussetcr@gmail.com>
2021-02-12 19:08:30 +0100

    jeu final

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 // DEPLACEMENT D’UN OBJET GRAPHIQUE
 // App.js
 import React, { useState } from 'react';
-import { View, Button, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Button, StyleSheet } from 'react-native';
 
 export default function App() {
   // variable d'état, setter de la variable d'état = valeur d'initiale
@@ -10,58 +10,93 @@ export default function App() {
   const [marginTop, setMarginTop] = useState(0);
   const [snakeX, setSnakeX] = useState(0);
   const [snakeY, setSnakeY] = useState(0);
-
+  // const [directionRight] = useState()
+  const foodX = '14vw';
+  const foodY= '14vw';
   
+  var apple = new Array(2);
+  apple[0] = Math.floor(Math.random() * 19) + 1;
+  apple[1] = Math.floor(Math.random() * 19) + 1; 
+
 
   var grid = new Array(25);
   for (var i = 0; i < 25; i++) {
     grid[i] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
   }
   grid[0][0] = 1;
+//pb dplacement snake
 
 
-  
-  const moveRight = () => {
-    if(snakeY < 24) {
-      setSnakeY(snakeY+1);
-      grid[snakeX][snakeY] = 1;
-      grid[snakeX][snakeY-1] = 0;
-      setMarginLeft(marginLeft + 2);
-      console.log(grid);
+
+  // function addApple()
+  // {
+  //     [0] = Math.floor(Math.random() * 19) + 1;
+  //     [1] = Math.floor(Math.random() * 19) + 1;
+  // }
+
+  function eatApple() {
+    if ([snakeX] == foodX && [snakeY] == foodY) {
+    alert("tu as mangé une pomme")
+
+
+
+      //rajouter les coordonnées de la pomme
+
+      setFood(getRandomFood);
     }
+  }
+
+
+
+
+  const moveRight = () => {
+   
+    if (snakeY < 24) {
+      setSnakeY(snakeY + 1);
+      grid[snakeX][snakeY] = 1;
+      grid[snakeX][snakeY - 1] = 0;
+      setMarginLeft(marginLeft + 2.8);
+
+      
+      console.log(grid);
+  
+  }
   };
 
   const moveLeft = () => {
-    if(snakeY > 0) {
-      setSnakeY(snakeY-1);
+  for (let i = 0; i < snakeX.length; i++) {
+    if (snakeY > 0) {
+      setSnakeY(snakeY - 1);
       grid[snakeX][snakeY] = 1;
-      grid[snakeX][snakeY+1] = 0;
-      setMarginLeft(marginLeft - 2);
+      grid[snakeX][snakeY + 1] = 0;
+      setMarginLeft(marginLeft - 2.8);
       console.log(grid);
     }
   };
+  }
+    
 
   const moveUp = () => {
-    if(snakeX > 0) {
-      setSnakeX(snakeX-1);
+    if (snakeX > 0) {
+      setSnakeX(snakeX - 1);
       grid[snakeX][snakeY] = 1;
-      grid[snakeX+1][snakeY] = 0;
-      setMarginTop(marginTop - 2);
+      grid[snakeX + 1][snakeY] = 0;
+      setMarginTop(marginTop - 2.8);
       console.log(grid);
     }
   };
 
   const moveDown = () => {
-    if(snakeX < 24) {
-      setSnakeX(snakeX+1);
+    if (snakeX < 24) {
+      setSnakeX(snakeX + 1);
       grid[snakeX][snakeY] = 1;
-      grid[snakeX-1][snakeY] = 0;
-      setMarginTop(marginTop + 2);
+      grid[snakeX - 1][snakeY] = 0;
+      setMarginTop(marginTop + 2.8);
       console.log(grid);
     }
   };
 
- 
+
 
   const styles = StyleSheet.create({
     gameArea: {
@@ -71,17 +106,27 @@ export default function App() {
       alignItems: 'center',
     },
     gameBox: {
-      width: '50vw',
-      height: '50vw',
+      width: '70vw',
+      height: '70vw',
       borderWidth: 2,
       borderColor: '#E0CF04'
     },
     snake: {
-      width: '2vw',
-      height: '2vw',
+      width: '2.8vw',
+      height: '2.8vw',
       backgroundColor: 'white',
       marginLeft: marginLeft + 'vw',
       marginTop: marginTop + 'vw'
+    },
+
+    food: {
+      width: '2.8vw',
+      height: '2.8vw',
+      backgroundColor: 'red',
+      position: 'absolute',
+      left: '14vw',
+      top: '14vw',
+
     },
 
     buttonLeft: {
@@ -92,7 +137,7 @@ export default function App() {
       left: '15%',
       bottom: '15%',
       borderRadius: "20%",
-    }, 
+    },
 
     buttonRight: {
       width: '15vw',
@@ -102,7 +147,7 @@ export default function App() {
       right: '15%',
       bottom: '15%',
       borderRadius: "20%",
-    }, 
+    },
 
     buttonUp: {
       width: '15vw',
@@ -124,14 +169,14 @@ export default function App() {
       bottom: '5%',
       borderRadius: "20%",
     },
-    
-    
+
+
 
 
 
 
   });
-  
+
 
   return (
     <View style={styles.gameArea}>
@@ -146,35 +191,35 @@ export default function App() {
       </View>
 
       <View style={styles.buttonRight}>
-      <Button
-        onPress={() => { moveRight() }}
-        title="➡"
-        color= '#0E0E0E'
-      />
+        <Button
+          onPress={() => { moveRight() }}
+          title="➡"
+          color='#0E0E0E'
+        />
       </View>
 
       <View style={styles.buttonLeft}>
-      <Button
-        onPress={() => { moveLeft() }}
-        title="⬅"
-        color= '#0E0E0E'
-      />
+        <Button
+          onPress={() => { moveLeft() }}
+          title="⬅"
+          color='#0E0E0E'
+        />
       </View>
 
       <View style={styles.buttonUp}>
-      <Button
-        onPress={() => { moveUp() }}
-        title=" ⬆ "
-        color= '#0E0E0E'
-      />
+        <Button
+          onPress={() => { moveUp() }}
+          title=" ⬆ "
+          color='#0E0E0E'
+        />
       </View>
 
       <View style={styles.buttonDown}>
-      <Button
-        onPress={() => { moveDown() }}
-        title="⬇"
-        color= '#0E0E0E'
-      />
+        <Button
+          onPress={() => { moveDown() }}
+          title="⬇"
+          color='#0E0E0E'
+        />
       </View>
     </View>
   );
@@ -190,3 +235,9 @@ const grosStyle = function (gauche) {
     marginLeft: gauche,
   }
 }
+
+// const deleteFood = function (delete) {
+//   return {
+//     display: 
+//   }
+// }
